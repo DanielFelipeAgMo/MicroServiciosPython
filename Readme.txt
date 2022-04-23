e1f6b6de07a3809c34cec6b7281c0976cadcebc4
DanielFelipeAgMo <danielagudelo87@gmail.com>
2022-04-23 15:36:59 -0500

    Ejercicio de un micro server

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+MICROSERVICIO BAJO EL ESTANDAR "WSGI" - Web Server Geteway Interface
+
+Es una especificación que describe cómo se comunica un servidor web con una aplicación web,
+ y cómo se pueden llegar a encadenar diferentes aplicaciones web para procesar una solicitud/petición (o request).
+
---
+++ microservicio.py
@@ -0,0 +1,19 @@
+import json
+from wsgiref.simple_server import make_server
+
+
+def application(environ, start_response):
+    headers = [('Content-Type', 'application/json')]
+
+    status = '200 OK'
+    output = 'Hola Desarrolladores TdeA estoy en el server!'
+
+    start_response(status, headers)
+
+    response = {'mensaje': output}
+
+    return [ bytes(json.dumps(response), 'utf8') ]
+
+
+server = make_server('localhost', 8000, application)
+server.handle_request()
